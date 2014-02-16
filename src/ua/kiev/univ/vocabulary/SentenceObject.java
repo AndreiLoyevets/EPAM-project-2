d44183fb6b0ffe30c4de94d5f1452e3188de5699
AndreiLoyevets <aloyevets@gmail.com>
2014-02-16 14:56:59 -0500

    проэкт 2 для ЕПАМ

---
+++ src/ua/kiev/univ/vocabulary/SentenceObject.java
@@ -0,0 +1,10 @@
+package ua.kiev.univ.vocabulary;
+
+/**
+ * This is a marker interface that means that the object of the class
+ * implementing SentenceObject can be an element of the Sentence.
+ * 
+ * @version 1.0 26 Nov 2013
+ * @author Andrei Loyevets
+ */
+public interface SentenceObject {}
---
+++ src/ua/kiev/univ/vocabulary/Symbol.java
@@ -0,0 +1,28 @@
+package ua.kiev.univ.vocabulary;
+
+/**
+ * This is the abstract class to store and manipulate symbols.
+ * 
+ * @version 1.0 26 Nov 2013
+ * @author Andrei Loyevets
+ *
+ */
+public abstract class Symbol {
+	private char symbol;
+	
+	/**
+	 * Creates new Symbol by the given character.
+	 * @param symbol the character to be stored
+	 */
+	protected Symbol(char symbol) {
+		this.symbol = symbol;
+	}
+	
+	/**
+	 * Returns the stored character.
+	 * @return the character representation of the Symbol
+	 */
+	public char getSymbol() {
+		return symbol;
+	}
+}
